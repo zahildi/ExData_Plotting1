6403faf73488036ba3b8e594e9418fa25724dc7d
David Michell <davemichell@gmail.com>
2015-06-06 17:31:31 +1000

    Updated file
    Comments added
    Option to send plot to screen

--- plot1.R
+++ plot1.R
@@ -3,21 +3,27 @@ plot1 <- function(){
     library("sqldf")
     
     ## down and dirty method of reading the required data
-    ## powerdata <- read.table("./data/household_power_consumption.txt",
+    ## reads all data into memory, then creates a subseet of the data
+    
+    ## Allpowerdata <- read.table("./data/household_power_consumption.txt",
     ##                  sep=";", header=TRUE)
-    ## reqPowerData <- subset(powerdata, Date == "1/2/2007" | Date=="2/2/2007")
-    ##rm(powerdata)
+    ## PowerData <- subset(powerdata, Date == "1/2/2007" | Date=="2/2/2007")
+    ## rm(powerdata)
 
     ## more elegant solution, using library sqldf
     PowerData <- read.csv.sql("./data/household_power_consumption.txt", 
                     sql= "select * from file where Date in ('1/2/2007', '2/2/2007')",
                     sep=";", header=TRUE)
     
-    png(file="plot1.png",width=480,height=480)
     
-    hist(reqPowerData2$Global_active_power, col="red", 
+    ## if line below is commented out, the plot will be sent to the screen
+    png(file="plot1.png",width=480,height=480) 
+    
+    hist(PowerData$Global_active_power, col="red", 
          main="Global Active Power", xlab="Global Active Power (kilowatts)")
     
+    ## if the plot is sent to the screen, uncomment below to copy to a file
+    ## dev.copy(png, file= "plot1.png") 
     dev.off()
     
     print("Plot saved as plot1.png in working directory")
